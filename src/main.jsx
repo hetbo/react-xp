727a19f7a4b8623cd16745125b632fba44c66519
Hosein Safdel <hetbo98@yahoo.com>
2025-08-21 13:29:44 +0000

    Clear the Project and Add TaiwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <div className="h-screen text-white p-4 w-screen bg-neutral-900">
+          <App />
+      </div>
   </StrictMode>,
 )
